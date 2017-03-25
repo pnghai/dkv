c752681d1c832513346e218fac9100a986e44f5d
Hai Phan Nguyen <pnghai@gmail.com>
2017-03-26 02:26:16 +0700

    add new home view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+<!-- Stored in resources/views/child.blade.php -->
+
+@extends('layouts.app')
+
+@section('title', 'Welcome to DKV')
+
+@section('content')
+    <p>This is my body content.</p>
+@endsection
